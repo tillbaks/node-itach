eaa7887cea304bfcef535edbbf81f077b9a6eb70
tillbaks <johannesbluml@gmail.com>
2016-05-20 14:26:00 +0200

    Removed examples. Now in README.

--- examples/2.js
+++
@@ -1,9 +0,0 @@
-var itach = require("../itach.js");
-
-itach.on("error", console.log);
-itach.on("debug", console.log);
-
-itach.discover(function(err, result){
-    console.log(result);
-    itach.close();
-});
