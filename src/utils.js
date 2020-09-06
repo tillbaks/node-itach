a0ceea446f93ee994ec8abd5364448920895e60a
tillbaks <johannes@bluml.se>
2020-09-06 16:24:53 +0200

    Update test libraries. Fix tests to work with new test libraries. Switch to prettier.

--- src/utils.js
+++ src/utils.js
@@ -3,63 +3,68 @@
  *
  * @return a promise that will be rejected when the timeout is reached otherwise the result of the passed promise
  */
-const timeoutPromise = ({promise, timeout, error}) => {
-  let timer = null
+const timeoutPromise = ({ promise, timeout, error }) => {
+  let timer = null;
 
   return Promise.race([
     new Promise((resolve, reject) => {
-      timer = setTimeout(reject, timeout, error)
-      return timer
+      timer = setTimeout(reject, timeout, error);
+      return timer;
     }),
     promise.then((value) => {
-      clearTimeout(timer)
-      return value
-    })
-  ])
-}
+      clearTimeout(timer);
+      return value;
+    }),
+  ]);
+};
 
 const createQueue = (taskFunc, concurrency = 1) => {
-  const queue = []
-  let active = 0
-  let paused = false
+  const queue = [];
+  let active = 0;
+  let paused = false;
 
   const run = async function () {
     if (paused || active >= concurrency || queue.length < 1) {
-      return
+      return;
     }
-    active += 1
-    const queueItem = queue.shift()
+    active += 1;
+    const queueItem = queue.shift();
     try {
       if (queueItem.timeout) {
-        queueItem.resolve(timeoutPromise({
-          promise: taskFunc(queueItem.task),
-          timeout: queueItem.timeout,
-          error: new Error('QueueTaskTimeout: Task failed to complete before timeout was reached.')
-        }))
+        queueItem.resolve(
+          timeoutPromise({
+            promise: taskFunc(queueItem.task),
+            timeout: queueItem.timeout,
+            error: new Error(
+              "QueueTaskTimeout: Task failed to complete before timeout was reached."
+            ),
+          })
+        );
       } else {
-        queueItem.resolve(taskFunc(queueItem.task))
+        queueItem.resolve(taskFunc(queueItem.task));
       }
     } catch (error) {
-      queueItem.reject(error)
+      queueItem.reject(error);
     } finally {
-      active -= 1
-      run()
+      active -= 1;
+      run();
     }
-  }
+  };
 
   return {
-    push: (task, timeout) => new Promise((resolve, reject) => {
-      queue.push({ task, resolve, reject, timeout })
-      run()
-    }),
+    push: (task, timeout) =>
+      new Promise((resolve, reject) => {
+        queue.push({ task, resolve, reject, timeout });
+        run();
+      }),
     pause: () => {
-      paused = true
+      paused = true;
     },
     resume: () => {
-      paused = false
-      run()
-    }
-  }
-}
+      paused = false;
+      run();
+    },
+  };
+};
 
-module.exports = { createQueue }
+module.exports = { createQueue };
--- test/itach.test.js
+++ test/itach.test.js
@@ -1,96 +1,101 @@
-const test = require('ava')
-const sinon = require('sinon')
-const itach = require('../')
+const test = require("ava");
+const sinon = require("sinon");
+const itach = require("../");
 
-test.cb.beforeEach(t => {
+test.beforeEach.cb((t) => {
   itach.setOptions({
-    host: '10.0.0.2',
+    host: "10.0.0.2",
     port: 4998,
     reconnect: true,
     reconnectSleep: 1000,
     sendTimeout: 500,
     retryInterval: 99,
-    connectionTimeout: 3000
-  })
-  t.deepEqual(itach.eventNames(), [])
-  itach.on('error', console.log)
-  t.end()
-})
+    connectionTimeout: 3000,
+  });
+  t.deepEqual(itach.eventNames(), []);
+  itach.on("error", console.log);
+  t.end();
+});
 
-test.cb.afterEach(t => {
-  itach.close({ reconnect: false })
+test.afterEach.cb((t) => {
+  itach.close({ reconnect: false });
   setTimeout(() => {
-    itach.removeAllListeners()
-    t.end()
-  }, 1000)
-})
+    itach.removeAllListeners();
+    t.end();
+  }, 1000);
+});
 
-test.serial.cb('can connect to itach device', t => {
-  t.plan(1)
+test.serial.cb("can connect to itach device", (t) => {
+  t.plan(1);
 
-  const connectFunc = sinon.spy()
+  const connectFunc = sinon.spy();
 
-  itach.on('connect', connectFunc)
+  itach.on("connect", connectFunc);
 
-  itach.connect()
-
-  setTimeout(() => {
-    t.is(connectFunc.callCount, 1)
-    t.end()
-  }, 3000)
-})
-
-test.serial.cb('connection times out', t => {
-  t.plan(3)
-
-  const connectFunc = sinon.spy()
-  const errorFunc = sinon.spy()
-
-  itach.on('connect', connectFunc)
-  itach.on('error', errorFunc)
-
-  itach.connect({ connectionTimeout: 10 })
+  itach.connect();
 
   setTimeout(() => {
-    t.is(connectFunc.callCount, 0)
-    t.is(errorFunc.callCount, 1)
-    t.is(errorFunc.getCall(0).args[0].message, 'Connection timeout.')
-    t.end()
-  }, 3000)
-})
+    t.is(connectFunc.callCount, 1);
+    t.end();
+  }, 10000);
+});
 
-test.serial.cb('sending sendir commands', t => {
-  t.plan(1)
+test.serial.cb("connection times out", (t) => {
+  t.plan(3);
 
-  itach.connect()
+  const connectFunc = sinon.spy();
+  const errorFunc = sinon.spy();
 
-  itach.on('connect', async () => {
-    const result = await itach.send('sendir,1:1,1,38400,1,1,347,173,22,22,22,65,22,22,22,22,22,65,22,22,22,22,22,22,22,22,22,22,22,65,22,22,22,65,22,65,22,22,22,22,22,22,22,22,22,65,22,22,22,22,22,22,22,22,22,22,22,65,22,65,22,22,22,65,22,65,22,65,22,65,22,65,22,1657')
-    t.is(result, 'completeir,1:1,1')
-    t.end()
-  })
-})
+  itach.on("connect", connectFunc);
+  itach.on("error", errorFunc);
 
-test.serial.cb('error when sending invalid sendir commands', t => {
-  t.plan(2)
+  itach.connect({ connectionTimeout: 10 });
 
-  itach.connect()
+  setTimeout(() => {
+    t.is(connectFunc.callCount, 0);
+    t.is(errorFunc.callCount, 1);
+    t.is(errorFunc.getCall(0).args[0].message, "Connection timeout.");
+    t.end();
+  }, 10000);
+});
 
-  itach.on('connect', async () => {
-    const error = await t.throws(itach.send('sendir:'), Error)
-    t.is(error.message, 'Invalid command. Command not found.')
-    t.end()
-  })
-})
+test.serial.cb("sending sendir commands", (t) => {
+  t.plan(1);
 
-test.serial.cb('error when sendtimeout reached', t => {
-  t.plan(2)
+  itach.connect();
 
-  itach.connect({ sendTimeout: 10 })
+  itach.on("connect", async () => {
+    const result = await itach.send(
+      "sendir,1:1,1,38400,1,1,347,173,22,22,22,65,22,22,22,22,22,65,22,22,22,22,22,22,22,22,22,22,22,65,22,22,22,65,22,65,22,22,22,22,22,22,22,22,22,65,22,22,22,22,22,22,22,22,22,22,22,65,22,65,22,22,22,65,22,65,22,65,22,65,22,65,22,1657"
+    );
+    t.is(result, "completeir,1:1,1");
+    t.end();
+  });
+});
 
-  itach.on('connect', async () => {
-    const error = await t.throws(itach.send('getdevices'), Error)
-    t.is(error.message, 'QueueTaskTimeout: Task failed to complete before timeout was reached.')
-    t.end()
-  })
-})
+test.serial.cb("error when sending invalid sendir commands", (t) => {
+  t.plan(2);
+
+  itach.connect();
+
+  itach.on("connect", async () => {
+    const error = await t.throwsAsync(itach.send("sendir:"), { instanceOf: Error });
+    t.is(error.message, "Invalid command. Command not found.");
+    t.end();
+  });
+});
+
+test.serial.cb("error when sendtimeout reached", (t) => {
+  t.plan(2);
+
+  itach.connect({ sendTimeout: 10 });
+
+  itach.on("connect", async () => {
+    const error = await t.throws(itach.send("getdevices"), Error);
+    t.is(
+      error.message,
+      "QueueTaskTimeout: Task failed to complete before timeout was reached."
+    );
+    t.end();
+  });
+});
